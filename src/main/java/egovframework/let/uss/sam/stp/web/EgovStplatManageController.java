5f6f7d15c6849d0ca4234ddc3b905ba5b6462fb8
kang <kfor11@naver.com>
2024-09-24 17:38:29 +0900

    Source > Format

--- src/main/java/egovframework/let/uss/sam/stp/web/EgovStplatManageController.java
+++ src/main/java/egovframework/let/uss/sam/stp/web/EgovStplatManageController.java
@@ -1,17 +1,10 @@
 package egovframework.let.uss.sam.stp.web;
 
-import egovframework.com.cmm.EgovMessageSource;
-import egovframework.com.cmm.LoginVO;
-import egovframework.let.uss.sam.stp.service.EgovStplatManageService;
-import egovframework.let.uss.sam.stp.service.StplatManageDefaultVO;
-import egovframework.let.uss.sam.stp.service.StplatManageVO;
+import javax.annotation.Resource;
 
 import org.egovframe.rte.fdl.property.EgovPropertyService;
 import org.egovframe.rte.fdl.security.userdetails.util.EgovUserDetailsHelper;
 import org.egovframe.rte.ptl.mvc.tags.ui.pagination.PaginationInfo;
-
-import javax.annotation.Resource;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -22,22 +15,29 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springmodules.validation.commons.DefaultBeanValidator;
 
+import egovframework.com.cmm.EgovMessageSource;
+import egovframework.com.cmm.LoginVO;
+import egovframework.let.uss.sam.stp.service.EgovStplatManageService;
+import egovframework.let.uss.sam.stp.service.StplatManageDefaultVO;
+import egovframework.let.uss.sam.stp.service.StplatManageVO;
+
 /**
  *
  * 약관내용을 처리하는 비즈니스 구현 클래스
+ * 
  * @author 공통서비스 개발팀 박정규
  * @since 2009.04.01
  * @version 1.0
  * @see
  *
- * <pre>
+ *      <pre>
  * << 개정이력(Modification Information) >>
  *
  *   수정일      수정자           수정내용
  *  -------    --------    ---------------------------
  *   2009.04.01  박정규          최초 생성
  *
- * </pre>
+ *      </pre>
  */
 @Controller
 public class EgovStplatManageController {
@@ -59,8 +59,9 @@ public class EgovStplatManageController {
 
 	/**
 	 * 개별 배포시 메인메뉴를 조회한다.
+	 * 
 	 * @param model
-	 * @return	"/uss/sam/stp/EgovMain"
+	 * @return "/uss/sam/stp/EgovMain"
 	 * @throws Exception
 	 */
 	@RequestMapping(value = "/uss/sam/stp/EgovMain.do")
@@ -70,8 +71,9 @@ public class EgovStplatManageController {
 
 	/**
 	 * 메뉴를 조회한다.
+	 * 
 	 * @param model
-	 * @return	"/uss/sam/stp/EgovLeft"
+	 * @return "/uss/sam/stp/EgovLeft"
 	 * @throws Exception
 	 */
 	@RequestMapping(value = "/uss/sam/stp/EgovLeft.do")
@@ -81,13 +83,15 @@ public class EgovStplatManageController {
 
 	/**
 	 * 약관정보 목록을 조회한다.
+	 * 
 	 * @param searchVO
 	 * @param model
-	 * @return	"/uss/sam/stp/EgovStplatListInqire"
+	 * @return "/uss/sam/stp/EgovStplatListInqire"
 	 * @throws Exception
 	 */
 	@RequestMapping(value = "/uss/sam/stp/StplatListInqire.do")
-	public String selectStplatList(@ModelAttribute("searchVO") StplatManageDefaultVO searchVO, ModelMap model) throws Exception {
+	public String selectStplatList(@ModelAttribute("searchVO") StplatManageDefaultVO searchVO, ModelMap model)
+			throws Exception {
 
 		/** EgovPropertyService.SiteList */
 		searchVO.setPageUnit(propertiesService.getInt("pageUnit"));
@@ -114,14 +118,16 @@ public class EgovStplatManageController {
 
 	/**
 	 * 약관정보상세내용을 조회한다.
+	 * 
 	 * @param stplatManageVO
 	 * @param searchVO
 	 * @param model
-	 * @return	"/uss/sam/stp/EgovStplatDetailInqire"
+	 * @return "/uss/sam/stp/EgovStplatDetailInqire"
 	 * @throws Exception
 	 */
 	@RequestMapping("/uss/sam/stp/StplatDetailInqire.do")
-	public String selectStplatDetail(StplatManageVO stplatManageVO, @ModelAttribute("searchVO") StplatManageDefaultVO searchVO, ModelMap model) throws Exception {
+	public String selectStplatDetail(StplatManageVO stplatManageVO,
+			@ModelAttribute("searchVO") StplatManageDefaultVO searchVO, ModelMap model) throws Exception {
 
 		StplatManageVO vo = stplatManageService.selectStplatDetail(stplatManageVO);
 
@@ -132,13 +138,15 @@ public class EgovStplatManageController {
 
 	/**
 	 * 약관정보를 등록하기 위한 전 처리
+	 * 
 	 * @param searchVO
 	 * @param model
-	 * @return	"/uss/sam/stp/EgovStplatCnRegist"
+	 * @return "/uss/sam/stp/EgovStplatCnRegist"
 	 * @throws Exception
 	 */
 	@RequestMapping("/uss/sam/stp/StplatCnRegistView.do")
-	public String insertStplatCnView(@ModelAttribute("searchVO") StplatManageDefaultVO searchVO, Model model) throws Exception {
+	public String insertStplatCnView(@ModelAttribute("searchVO") StplatManageDefaultVO searchVO, Model model)
+			throws Exception {
 
 		model.addAttribute("stplatManageVO", new StplatManageVO());
 
@@ -148,15 +156,17 @@ public class EgovStplatManageController {
 
 	/**
 	 * 약관정보를 등록한다.
+	 * 
 	 * @param searchVO
 	 * @param stplatManageVO
 	 * @param bindingResult
-	 * @return	"forward:/uss/sam/stp/StplatListInqire.do"
+	 * @return "forward:/uss/sam/stp/StplatListInqire.do"
 	 * @throws Exception
 	 */
 	@RequestMapping("/uss/sam/stp/StplatCnRegist.do")
-	public String insertStplatCn(@ModelAttribute("searchVO") StplatManageDefaultVO searchVO, @ModelAttribute("stplatManageVO") StplatManageVO stplatManageVO,
-			BindingResult bindingResult) throws Exception {
+	public String insertStplatCn(@ModelAttribute("searchVO") StplatManageDefaultVO searchVO,
+			@ModelAttribute("stplatManageVO") StplatManageVO stplatManageVO, BindingResult bindingResult)
+			throws Exception {
 
 		beanValidator.validate(stplatManageVO, bindingResult);
 
@@ -166,7 +176,7 @@ public class EgovStplatManageController {
 
 		}
 
-		// 로그인VO에서  사용자 정보 가져오기
+		// 로그인VO에서 사용자 정보 가져오기
 		LoginVO loginVO = (LoginVO) EgovUserDetailsHelper.getAuthenticatedUser();
 
 		String frstRegisterId = loginVO.getUniqId();
@@ -181,14 +191,16 @@ public class EgovStplatManageController {
 
 	/**
 	 * 약관정보를 수정하기 위한 전 처리
+	 * 
 	 * @param useStplatId
 	 * @param searchVO
 	 * @param model
-	 * @return	"/uss/sam/stp/EgovStplatCnUpdt"
+	 * @return "/uss/sam/stp/EgovStplatCnUpdt"
 	 * @throws Exception
 	 */
 	@RequestMapping("/uss/sam/stp/StplatCnUpdtView.do")
-	public String updateStplatCnView(@RequestParam("useStplatId") String useStplatId, @ModelAttribute("searchVO") StplatManageDefaultVO searchVO, ModelMap model) throws Exception {
+	public String updateStplatCnView(@RequestParam("useStplatId") String useStplatId,
+			@ModelAttribute("searchVO") StplatManageDefaultVO searchVO, ModelMap model) throws Exception {
 
 		StplatManageVO stplatManageVO = new StplatManageVO();
 
@@ -206,15 +218,17 @@ public class EgovStplatManageController {
 
 	/**
 	 * 약관정보를 수정 처리한다.
+	 * 
 	 * @param searchVO
 	 * @param stplatManageVO
 	 * @param bindingResult
-	 * @return	"forward:/uss/sam/stp/StplatListInqire.do"
+	 * @return "forward:/uss/sam/stp/StplatListInqire.do"
 	 * @throws Exception
 	 */
 	@RequestMapping("/uss/sam/stp/StplatCnUpdt.do")
-	public String updateStplatCn(@ModelAttribute("searchVO") StplatManageDefaultVO searchVO, @ModelAttribute("stplatManageVO") StplatManageVO stplatManageVO,
-			BindingResult bindingResult) throws Exception {
+	public String updateStplatCn(@ModelAttribute("searchVO") StplatManageDefaultVO searchVO,
+			@ModelAttribute("stplatManageVO") StplatManageVO stplatManageVO, BindingResult bindingResult)
+			throws Exception {
 
 		// Validation
 		beanValidator.validate(stplatManageVO, bindingResult);
@@ -225,7 +239,7 @@ public class EgovStplatManageController {
 
 		}
 
-		// 로그인VO에서  사용자 정보 가져오기
+		// 로그인VO에서 사용자 정보 가져오기
 		LoginVO loginVO = (LoginVO) EgovUserDetailsHelper.getAuthenticatedUser();
 
 		String lastUpdusrId = loginVO.getUniqId();
@@ -239,13 +253,15 @@ public class EgovStplatManageController {
 
 	/**
 	 * 약관정보를 삭제 처리한다.
+	 * 
 	 * @param stplatManageVO
 	 * @param searchVO
-	 * @return	"forward:/uss/sam/stp/StplatListInqire.do"
+	 * @return "forward:/uss/sam/stp/StplatListInqire.do"
 	 * @throws Exception
 	 */
 	@RequestMapping("/uss/sam/stp/StplatCnDelete.do")
-	public String deleteStplatCn(StplatManageVO stplatManageVO, @ModelAttribute("searchVO") StplatManageDefaultVO searchVO) throws Exception {
+	public String deleteStplatCn(StplatManageVO stplatManageVO,
+			@ModelAttribute("searchVO") StplatManageDefaultVO searchVO) throws Exception {
 
 		stplatManageService.deleteStplatCn(stplatManageVO);
 
