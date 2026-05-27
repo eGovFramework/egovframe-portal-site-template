9f52e16d4aff2b23d0a31d04bdbec10ca4fcabd3
dasomel <dasomell@gmail.com>
2026-05-28 00:41:18 +0900

    refactor: com/cmm 패키지 VO Lombok @Getter/@Setter 적용
    IncludedCompInfoVO, SessionVO의 수동 작성 getter/setter를
    Lombok @Getter/@Setter 어노테이션으로 대체하여 보일러플레이트 코드 제거.
    기존 메서드 시그니처 및 동작은 동일하게 유지됨.

--- src/main/java/egovframework/com/cmm/IncludedCompInfoVO.java
+++ src/main/java/egovframework/com/cmm/IncludedCompInfoVO.java
@@ -1,5 +1,8 @@
 package egovframework.com.cmm;
 
+import lombok.Getter;
+import lombok.Setter;
+
 /**
  * IncludedInfo annotation을 바탕으로 화면에 표시할 정보를 구성하기 위한 VO 클래스
  * @author 공통컴포넌트 정진오
@@ -9,42 +12,20 @@ package egovframework.com.cmm;
  *
  * <pre>
  * << 개정이력(Modification Information) >>
- *   
+ *
  *  수정일		수정자		수정내용
  *  -------    	--------    ---------------------------
  *  2011.08.26	정진오 		최초 생성
  *
  * </pre>
  */
+@Getter
+@Setter
 public class IncludedCompInfoVO {
-	
+
 	private String name;
 	private String listUrl;
 	private int order;
 	private int gid;
-	
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
-	public String getListUrl() {
-		return listUrl;
-	}
-	public void setListUrl(String listUrl) {
-		this.listUrl = listUrl;
-	}
-	public int getOrder() {
-		return order;
-	}
-	public void setOrder(int order) {
-		this.order = order;
-	}
-	public int getGid() {
-		return gid;
-	}
-	public void setGid(int gid) {
-		this.gid = gid;
-	}
+
 }
