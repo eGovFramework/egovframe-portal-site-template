913cd099db259aadca2b4124930ca824ffbec0bc
이백행 <dlqorgod@naver.com>
2026-08-25 20:51:01 +0900

    test: AssertJ 3.27.7 테스트 의존성 추가

--- src/test/java/egovframework/test/EgovTestAbstractSpring.java
+++ src/test/java/egovframework/test/EgovTestAbstractSpring.java
@@ -44,15 +44,19 @@ import lombok.extern.slf4j.Slf4j;
 
 @ImportResource({
 
-		"classpath*:egovframework/spring/com/test-context-common.xml",
+//		"classpath*:egovframework/spring/com/test-context-common.xml",
 		"classpath*:egovframework/spring/com/test-context-egovuserdetailshelper.xml",
+//
+//		"classpath*:egovframework/spring/com/context-crypto.xml",
+//		"classpath*:egovframework/spring/com/context-datasource.xml",
+////		"classpath*:egovframework/spring/com/context-egovuserdetailshelper.xml",
+//		"classpath*:egovframework/spring/com/context-mapper.xml",
+//		"classpath*:egovframework/spring/com/context-properties.xml",
+//		"classpath*:egovframework/spring/com/context-transaction.xml",
 
-		"classpath*:egovframework/spring/com/context-crypto.xml",
-		"classpath*:egovframework/spring/com/context-datasource.xml",
-//		"classpath*:egovframework/spring/com/context-egovuserdetailshelper.xml",
-		"classpath*:egovframework/spring/com/context-mapper.xml",
-		"classpath*:egovframework/spring/com/context-properties.xml",
-		"classpath*:egovframework/spring/com/context-transaction.xml",
+		"classpath*:egovframework/spring/com/context-*.xml",
+
+		"file:src/main/webapp/WEB-INF/config/egovframework/springmvc/*.xml",
 
 })
 
